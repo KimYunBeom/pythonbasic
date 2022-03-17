3c4a883326b2a7de221080a2c777ec61e96e38d7
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-17 10:35:49 +0900

    문자열처리함수

--- practice.py
+++ practice.py
@@ -1,10 +1,18 @@
-jumin = "990120-1234567"
+python = "Python is Amazing"
+print(python.lower())
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "Java"))
 
-print("성별 : " + jumin[7])
-print("연 : " + jumin[0:2])  # 0 부터 2 직전까지 (0, 1)
-print("월 : " + jumin[2:4])
-print("일 : " + jumin[4:6])
+index = python.index("n")
+print(index)
 
-print("생년월일 : " + jumin[:6])  # 처음부터 6 직전까지
-print("뒤 7자리 : " + jumin[7:])  # 7 부터 마지막까지
-print("뒤 7자리 (뒤에부터): " + jumin[-7:])  # 맨 뒤의 -7번째부터 끝까지
+index = python.index("n", index + 1)
+print(index)
+
+print(python.find("Java"))
+# print(python.index("Java"))
+print("hi")
+
+print(python.count("n"))
