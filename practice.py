ae48748d2860d39a19764afa8bf19ea4d61e7cae
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-17 15:58:21 +0900

    기본값

--- practice.py
+++ practice.py
@@ -1,25 +1,15 @@
-def open_account():
-  print("새로운 계좌가 생성되었습니다.")
+# def profile(name, age, main_lang): 
+#   print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}" \
+#     .format(name, age, main_lang))
 
-def deposit(balance, money): # 입금
-  print("입금이 완료되었습니다. 잔액은 {0}원 입니다.".format(balance + money))
-  return balance + money
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
 
-def withdraw(balance, money): #출금
-  if (balance > money): # 잔액이 출금보다 많으면
-    print("출금이 완료되었습니다. 잔액은 {0}원 입니다.".format(balance - money))
-    return balance - money
-  else: 
-    print("출금이 완료되지 않았습니다. 잔액은 {0}원 입니다.".format(balance))
-    return balance
+# 같은 학교, 같은 학년, 같은 반, 같은 수업
 
-def withdraw_night(balance, money): #저녁에 출금
-  commission = 100 # 수수료
-  return commission, balance - money - commission
+def profile(name, age=17, main_lang="파이썬"): 
+  print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}" \
+    .format(name, age, main_lang))
 
-balance = 0 # 잔액
-balance = deposit(balance, 1000)
-# balance = withdraw(balance, 2000)
-# balance = withdraw(balance, 500)
-commission, balance = withdraw_night(balance, 500)
-print("수수료 {0}원이며, 잔액은 {1}원 입니다.".format(commission, balance))
+profile("유재석")
+profile("김태호")
