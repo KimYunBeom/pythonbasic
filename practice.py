ebb1df48a8687e3b9d262f1b7502659a72e383e6
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-17 10:25:21 +0900

    퀴즈 #1

--- practice.py
+++ practice.py
@@ -1,18 +1,14 @@
-# 애완동물을 소개해 주세요~
-animal = "고양이"
-name = "해피"
-age = 4
-hobby = "낮잠"
-is_adult = age >= 3
-
-'''이렇게
-하면
-여러 문장이
-주석처리 됩니다
+'''
+Quiz) 변수를 이용하여 다음 문장을 출력하시오
+변수명
+ : station
+변수값
+ : "사당", "신도림", "인천공항" 순서대로 입력
+출력 문장
+ : XX 행 열차가 들어오고 있습니다.
 '''
 
-print("우리집 " + animal + "의 이름은 " + name + "이예요")
-hobby = "공놀이"
-# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
-print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
-print(name + "는 어른일까요? " + str(is_adult))
+# station = "사당"
+# station = "신도림"
+station = "인천공항"
+print(station, "행 열차가 들어오고 있습니다")
