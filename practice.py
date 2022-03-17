d0d2af7859513332462883edda834fe233952fa9
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-17 10:30:44 +0900

    랜덤함수

--- practice.py
+++ practice.py
@@ -1,6 +1,34 @@
-print(abs(-5)) # 5
-print(pow(4, 2)) # 4^2 = 4*4 = 16
-print(max(5, 12)) # 12
-print(min(5, 12)) # 5
-print(round(3.14)) # 3
-print(round(4.99)) # 5
+from random import *
+
+# print(random()) # 0.0 이상 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0.0 이상 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
