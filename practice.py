88ae571f95af02339f4400225e0b3fb5e0579959
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-17 10:37:38 +0900

    퀴즈 #3

--- practice.py
+++ practice.py
@@ -1,20 +1,20 @@
-# \n : 줄바꿈
-# print("백문이 불여일견 \n백견이 불여일타")
+'''
+Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+예) http://naver.com
+규칙1 : http:// 부분은 제외 => naver.com
+규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!" 로 구성
+                  (nav)                (5)             (1)         (!)
+예) 생성된 비밀번호 : nav51!
+'''
 
-# \" \' : 문장 내에서 따옴표
-# 저는 "나도코딩"입니다.
-# print("저는 '나도코딩'입니다.")
-# print('저는 "나도코딩"입니다.')
-# print("저는 \"나도코딩\"입니다.")
-
-# \\ : 문장 내에서 \
-# print("C:\\data\\dev\\workspace\\private-study\\inflearn")
-
-# \r : 커서를 맨 앞으로 이동
-# print("Red Apple\rPine")
-
-# \b : 백스페이스 (한 글자 삭제)
-# print("Redd\bApple")
-
-# \t : 탭
-print("Red\tApple")
+# url = "http://naver.com"
+# url = "http://google.com"
+url = "http://youtube.com"
+my_str = url.replace("http://", "") # 규칙 1
+# print(my_str)
+my_str = my_str[:my_str.index(".")] # 규칙 2
+# my_str[0:5] -> 0 ~ 5 직전까지. (0, 1, 2, 3, 4)
+# print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print(f"{url} 의 비밀번호는 {password} 입니다.")
