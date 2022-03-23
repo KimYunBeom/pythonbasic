e67c184d2bc30207806d8b1f8a070980afb4fa8f
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-23 19:07:37 +0900

    pickle

--- practice.py
+++ practice.py
@@ -1,41 +1,14 @@
-# 'w' : overwrite
-# score_file = open('score.txt', 'w', encoding='utf-8')
-# print('수학 : 0', file=score_file)
-# print('영어 : 50', file=score_file)
-# score_file.close()
+import pickle
 
-# 'a' : append
-# score_file = open('score.txt', 'a', encoding='utf-8')
-# score_file.write('과학 : 80')
-# score_file.write('\n')
-# score_file.write('코딩 : 100')
-# score_file.close()
+# write binary
+# profile_file = open('profile.pickle', 'wb')
+# profile = {"이름":"박명수", "나이":30, "취미": ["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) # 변수 정보를 파일에 저장
+# profile_file.close()
 
-# 'r' : read1
-# score_file = open('score.txt', 'r', encoding='utf-8')
-# print(score_file.read())
-# score_file.close()
-
-# 'r' : read2
-# score_file = open('score.txt', 'r', encoding='utf-8')
-# print(score_file.readline(), end='') # 줄별로 읽기, 읽은 후 다음 줄로 커서 이동
-# print(score_file.readline(), end='')
-# print(score_file.readline(), end='')
-# print(score_file.readline(), end='')
-# score_file.close()
-
-# 'r' : read3
-# score_file = open('score.txt', 'r', encoding='utf-8')
-# while True:
-#   line = score_file.readline()
-#   if not line:
-#     break
-#   print(line, end='')
-# score_file.close()
-
-# 'r' : read4
-score_file = open('score.txt', 'r', encoding='utf-8')
-lines = score_file.readlines() # list로 저장
-for line in lines:
-  print(line, end='')
-score_file.close()
+# read binary
+profile_file = open('profile.pickle', 'rb')
+profile = pickle.load(profile_file) # 파일의 정보를 변수에 불러오기
+print(profile)
+profile_file.close()
