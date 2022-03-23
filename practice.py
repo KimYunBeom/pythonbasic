7569476675b63a20e6fba1e3daa92174e75ca925
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-23 19:21:14 +0900

    퀴즈 #7

--- practice.py
+++ practice.py
@@ -1,11 +1,20 @@
-# import pickle
-# with open('profile.pickle', 'rb') as profile_file:
-#   print(pickle.load(profile_file))
+'''
+Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
 
-# write
-# with open('study.txt', 'w', encoding='utf8') as study_file:
-#   study_file.write('파이썬을 열심히 공부하고 있어요')
+- X 주차 주간보고 - 
+부서 : 
+이름 : 
+업무 요약 : 
 
-# read
-with open('study.txt', 'r', encoding='utf8') as study_file:
-  print(study_file.read())
+1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
+
+조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
+'''
+
+for cnt in range(1, 51):
+  with open('{0}주차.txt'.format(cnt), 'w', encoding='utf8') as report_file:
+    report_file.write('- {0} 주차 주간보고 -\n' \
+                      '부서 : \n' \
+                      '이름 : \n' \
+                      '업무 요약 : '.format(cnt))
