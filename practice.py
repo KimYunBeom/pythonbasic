6e51c27e0cee530a6fa091b67fda6a00bfdd891a
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-23 19:02:34 +0900

    파일입출력

--- practice.py
+++ practice.py
@@ -1,26 +1,41 @@
-# 빈 자리는 빈 공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
-# print("{0: >10}".format(500))
+# 'w' : overwrite
+# score_file = open('score.txt', 'w', encoding='utf-8')
+# print('수학 : 0', file=score_file)
+# print('영어 : 50', file=score_file)
+# score_file.close()
 
-# 양수일 땐 +로 표시, 음수일 땐 -로 표시
-# print("{0: >+10}".format(500))
-# print("{0: >+10}".format(-500))
+# 'a' : append
+# score_file = open('score.txt', 'a', encoding='utf-8')
+# score_file.write('과학 : 80')
+# score_file.write('\n')
+# score_file.write('코딩 : 100')
+# score_file.close()
 
-# 왼쪽 정렬하고, 빈칸으로 _로 채움
-# print("{0:_<10}".format(500))
+# 'r' : read1
+# score_file = open('score.txt', 'r', encoding='utf-8')
+# print(score_file.read())
+# score_file.close()
 
-# 3자리 마다 콤마를 찍어주기
-# print("{0:,}".format(100000000000))
+# 'r' : read2
+# score_file = open('score.txt', 'r', encoding='utf-8')
+# print(score_file.readline(), end='') # 줄별로 읽기, 읽은 후 다음 줄로 커서 이동
+# print(score_file.readline(), end='')
+# print(score_file.readline(), end='')
+# print(score_file.readline(), end='')
+# score_file.close()
 
-# 3자리 마다 콤마를 찍어주기, +- 부호도 붙이기
-# print("{0:+,}".format(100000000000))
-# print("{0:+,}".format(-100000000000))
+# 'r' : read3
+# score_file = open('score.txt', 'r', encoding='utf-8')
+# while True:
+#   line = score_file.readline()
+#   if not line:
+#     break
+#   print(line, end='')
+# score_file.close()
 
-# 3자리 마다 콤마를 찍어주기, +- 부호도 붙이고, 자릿수 확보하기
-# 돈이 많으면 행복하니까 빈 자리는 ^ 로 채워주기
-# print("{0:^<+30,}".format(100000000000))
-
-# 소수점 출력
-print("{0:f}".format(5/3))
-
-# 소수점 특정 자리수까지만출력 (소수점 3째 자리에서 반올림)
-print("{0:.2f}".format(5/3))
+# 'r' : read4
+score_file = open('score.txt', 'r', encoding='utf-8')
+lines = score_file.readlines() # list로 저장
+for line in lines:
+  print(line, end='')
+score_file.close()
