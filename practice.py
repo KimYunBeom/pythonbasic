1c7baa18b9098d6ade5e594ad61432b51464fe60
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-24 15:49:48 +0900

    클래스

--- practice.py
+++ practice.py
@@ -1,20 +1,42 @@
-'''
-Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
-보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# name = '마린' # 유닛의 이름
+# hp = 40 # 유닛의 체력
+# damage = 5 # 유닛의 공격력
 
-- X 주차 주간보고 - 
-부서 : 
-이름 : 
-업무 요약 : 
+# print('{} 유닛이 생성되었습니다.'.format(name))
+# print('체력 {0}, 공격력 {1}\n'.format(hp, damage))
 
-1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+# tank_name = '탱크'
+# tank_hp = 150
+# tank_damage = 35
 
-조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
-'''
+# print('{} 유닛이 생성되었습니다.'.format(tank_name))
+# print('체력 {0}, 공격력 {1}\n'.format(tank_hp, tank_damage))
 
-for cnt in range(1, 51):
-  with open('{0}주차.txt'.format(cnt), 'w', encoding='utf8') as report_file:
-    report_file.write('- {0} 주차 주간보고 -\n' \
-                      '부서 : \n' \
-                      '이름 : \n' \
-                      '업무 요약 : '.format(cnt))
+# tank2_name = '탱크'
+# tank2_hp = 150
+# tank2_damage = 35
+
+# print('{} 유닛이 생성되었습니다.'.format(tank2_name))
+# print('체력 {0}, 공격력 {1}\n'.format(tank2_hp, tank2_damage))
+
+# def attack(name, location, damage):
+#   print('{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]'.format( \
+#     name, location, damage))
+
+# attack(name, '1시', damage)
+# attack(tank_name, '1시', tank_damage)
+# attack(tank2_name, '1시', tank2_damage)
+
+class Unit: 
+  def __init__(self, name, hp, damage) :
+    self.name = name
+    self.hp = hp
+    self.damage = damage
+    print('{} 유닛이 생성되었습니다.'.format(self.name))
+    print('체력 {0}, 공격력 {1}\n'.format(self.hp, self.damage))
+
+marine1 = Unit('마린', 40, 5)
+marine2 = Unit('마린', 40, 5)
+tank = Unit('탱크', 150, 35)
