61a10f635b6e79292ab08a7b130bc7631b7d6873
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-03-24 17:23:12 +0900

    메소드 오버라이딩

--- practice.py
+++ practice.py
@@ -1,13 +1,19 @@
 # 일반 유닛
 class Unit:
-  def __init__(self, name, hp):
+  def __init__(self, name, hp, speed):
     self.name = name
     self.hp = hp
+    self.speed = speed
+  
+  def move(self, location):
+    print('[지상 유닛 이동]')
+    print('{0} : {1} 방향으로 이동합니다. [속도 {2}]'\
+      .format(self.name, location, self.speed))
 
 # 공격 유닛
 class AttackUnit(Unit):
-  def __init__(self, name, hp, damage):
-    Unit.__init__(self, name, hp)
+  def __init__(self, name, hp, speed, damage):
+    Unit.__init__(self, name, hp, speed)
     self.damage = damage
 
   def attack(self, location):
@@ -21,8 +27,6 @@ class AttackUnit(Unit):
     if self.hp <= 0:
       print('{0} : 파괴되었습니다.'.format(self.name))
 
-# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
-
 # 날 수 있는 기능을 가진 클래스
 class Flyable: 
   def __init__(self, flying_speed):
@@ -35,9 +39,18 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
   def __init__(self, name, hp, damage, flying_speed):
-    AttackUnit.__init__(self, name, hp, damage)
+    AttackUnit.__init__(self, name, hp, 0, damage)
     Flyable.__init__(self, flying_speed)
 
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
-valkyrie = FlyableAttackUnit('발키리', 200, 6, 5)
-valkyrie.fly(valkyrie.name, '3시')
+  def move(self, location):
+    print('[공중 유닛 이동]')
+    self.fly(self.name, location)
+
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit('벌쳐', 80, 10, 20)
+
+# 배틀크루저 : 공중 유닛, 체력과 공격력이 좋음
+battlecruiser = FlyableAttackUnit('배틀크루저', 500, 25, 3)
+
+vulture.move('11시')
+battlecruiser.move('9시')
